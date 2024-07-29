b1d197ebe1bae452f824a428232178feb8a2a1e0
Thorsten Roeske <thorsten@wearonize.com>
2024-07-29 14:06:13 +0000

    add: calculate surplus

--- run.py
+++ run.py
@@ -68,7 +68,7 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated\n")
 
-def calculate_surplus_data(sales_data):
+def calculate_surplus_data(sales_row):
     """
     Compare sales with stock and calculate surplus 
     - Positive number indicates waste
@@ -79,7 +79,13 @@ def calculate_surplus_data(sales_data):
 
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
+    
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    
+    return surplus_data
 
 
 
@@ -92,7 +98,9 @@ def main():
     sales_data = [int(num) for num in data]
 
     update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
+    new_surplus = calculate_surplus_data(sales_data)
+
+    print(new_surplus)
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
